e0ae6e70f06901f23a212df1cf1ab3f87ce7824d
Green <47804536+YoungShisui@users.noreply.github.com>
2025-09-23 01:28:05 +0000

    leaflet

--- scripts.js
+++ scripts.js
@@ -30,3 +30,16 @@ var mods = ["Acute","Adaptive","Aegis","Black Armory","Bold","Brave","Breaker","
  var d = $(this).find( "option:selected" ).text();
  $(this).next("input[type='text']").val(d);
 });
+
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+
+map.addLayer(layer);
