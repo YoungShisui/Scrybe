2dd465342c7879082a8c562dcc1a9037a7df707f
Green <47804536+YoungShisui@users.noreply.github.com>
2025-11-02 21:53:17 +0000

    searh weapon mod table

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+//Tool Tips
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('[data-tooltip-id]').forEach(el => {
     const id = el.getAttribute('data-tooltip-id');
@@ -26,10 +28,38 @@ selector.addEventListener('change', (event) => {
  $(this).next("input[type='text']").val(d);
 });
 
+//Campaign Diary
+
 function changeText01(text) {
   document.getElementById('session01').innerHTML=text;
 }
 
 function changeText02(text) {
   document.getElementById('session02').innerHTML=text;
+}
+
+function changeText03(text) {
+  document.getElementById('session03').innerHTML=text;
+}
+
+
+// Searchable Table
+function modFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("modInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("modTable");
+  tr = table.getElementsByTagName("tr");
+  
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
 }
